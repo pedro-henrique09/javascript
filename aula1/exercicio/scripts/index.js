d2ef71c3b649fabf8082587e68e05ed38852b1eb
Pedro Henrique <pedrorj1990@hotmail.com>
2023-06-21 18:23:59 -0300

    subindo exercicio aula 1

---
+++ aula1/exercicio/scripts/index.js
@@ -0,0 +1,16 @@
+let nomeInserido = prompt("Insira seu nome");
+console.log(nomeInserido);
+
+let sobrenome = prompt("Insira seu sobrenome");
+console.log(sobrenome);
+
+let nomeSobrenome = "Olá, " + nomeInserido + " " + sobrenome;
+alert(nomeSobrenome);
+
+let a = prompt("Digite a sua idade");
+
+let b = prompt("Digite a idade de sua mãe");
+
+let soma = parseInt(a) + parseInt(b);
+let mensagem = "A soma das idades é: " + soma;
+alert(mensagem);
